989af566210ebd8fe330b9f741522eddf35b6428
Alex Raul Santo Araujo <06562980143@caltins.local>
2020-06-29 14:23:15 -0300

    Corrigindo problemas nas @media do css

--- src/Components/Section3/Section3.jsx
+++ src/Components/Section3/Section3.jsx
@@ -10,7 +10,7 @@ export default function section3() {
     return (
         <div className="container-fluid section3">
             <div className="row d-flex justify-content-center carousel">
-                <img src={Store} alt="casa" className="col-md-6 store xs-12 " />
+                <img src={Store} alt="casa" className="col-md-6 store col-12 " />
                 <div className=" col-md-3 col-12 row ">
                     <h1 className="text-center">Sabe aquela ideia que você teve para seu négocio?<EmojiObjectsOutlinedIcon className="ml-2" style={{ fontSize: 40 }} color="secondary" /></h1>
                     <p >A <strong>Evolution</strong> com a sua equipe de programadores estão a mil para trazer para ao mundo real e garantir que o seu negócio cresça acompanhando o mundo da tecnologia.</p>
--- src/Components/Section5/Section5.jsx
+++ src/Components/Section5/Section5.jsx
@@ -10,29 +10,29 @@ import Vue from '../../Assets/vue.svg'
 export default function section4() {
     return (
         <div className="container section5 bg-light mb-5">
-            <h1 className="text-center mb-5">Utilizamos os melhores frameworks e blibiotecas para desenvolvimento web.</h1>
+            <h1 className="text-center mb-5 ">Utilizamos os melhores frameworks e blibiotecas para desenvolvimento web.</h1>
             <div className="row justify-content-between  ">
-                <div className="col-3 mr-1 mb-5 circle ">
+                <div className="col-md-3 col-12 mr-1 ml-1 mb-md-5 mb-2 circle ">
                     <img src={Angular} alt="angular" className="frameworks" />
 
                 </div>
-                <div className="col-3 mr-1 mb-5 circle">
+                <div className="col-md-3 col-12 mr-1 ml-1 mb-md-5 mb-2 circle">
                     <img src={Reagir} alt="react" className="frameworks" />
 
                 </div>
-                <div className="col-3 mr-1 mb-5 circle">
+                <div className="col-md-3 col-12 mr-1 ml-1 mb-md-5 mb-2 circle">
                     <img src={Vue} alt="vuejs" className="frameworks" />
 
                 </div>
-                <div className="col-3 mr-1 mb-5 circle">
+                <div className="col-md-3 col-12 mr-1 ml-1 mb-md-5 mb-2 circle">
                     <img src={Node} alt="nodejs" className="frameworks" />
 
                 </div>
-                <div className="col-3 mr-1 mb-5 circle">
+                <div className="col-md-3 col-12 mr-1 ml-1 mb-md-5 mb-2 circle">
                     <img src={Js} alt="js" className="frameworks" />
 
                 </div>
-                <div className="col-3 mr-1 mb-5 circle">
+                <div className="col-md-3 col-12 mr-1 ml-1 mb-sm-5 circle">
                     <img src={Python} alt="python" className="frameworks" />
 
                 </div>
